85d59d851857956f7d6d7cdcdc518bd634ea411b
Johnny Chang <zjps3407@gmail.com>
2021-03-30 16:51:22 +0800

    Updates

---
+++ precache-manifest.ff2810422dcff2299610f8738360ce43.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3faa60c350a38aa48a9e585208030e8f",
+    "url": "/github.io/auction-serverless-frontend/index.html"
+  },
+  {
+    "revision": "6fc4f7eebd31e7668ec4",
+    "url": "/github.io/auction-serverless-frontend/static/css/main.854a357d.chunk.css"
+  },
+  {
+    "revision": "043fef5aa05219d23fb8",
+    "url": "/github.io/auction-serverless-frontend/static/js/2.89ed2bf2.chunk.js"
+  },
+  {
+    "revision": "5727441246ffa49345a3574f984c2034",
+    "url": "/github.io/auction-serverless-frontend/static/js/2.89ed2bf2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6fc4f7eebd31e7668ec4",
+    "url": "/github.io/auction-serverless-frontend/static/js/main.a0723f38.chunk.js"
+  },
+  {
+    "revision": "1275ed0d9d94ef51ad18",
+    "url": "/github.io/auction-serverless-frontend/static/js/runtime-main.728f1a90.js"
+  }
+]);
